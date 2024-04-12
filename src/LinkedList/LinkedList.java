042ae935a06d88d1285188edcacb6b41c5a27007
Nick Adams <nick.pappas.adams@gmail.com>
2024-04-12 11:42:24 -0700

    consstructor and print methods

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,36 @@
+package LinkedList;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void printTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void printLength() {
+        System.out.println("Length: " + length);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import LinkedList.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
+
+        LinkedList list = new LinkedList(1);
+        list.printList();
+        list.printHead();
+        list.printTail();
     }
 }
