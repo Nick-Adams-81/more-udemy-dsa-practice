efde19d0a26f554d2a478076073444ba561837a2
Nick Adams <nick.pappas.adams@gmail.com>
2024-04-16 17:34:05 -0700

    hash table consructor

---
+++ src/HashTable/HashTable.java
@@ -0,0 +1,13 @@
+package HashTable;
+
+public class HashTable {
+
+    private int size = 7;
+    private Node[] dataMap;
+
+    public HashTable() {
+        dataMap = new Node[size];
+    }
+
+
+}
---
+++ src/HashTable/Node.java
@@ -0,0 +1,13 @@
+package HashTable;
+
+public class Node {
+
+    String key;
+    int value;
+    Node next;
+
+    public Node(String key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
